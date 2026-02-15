faf665abaf2fbcb253976863ee978d2a251dc442
ann-ost <ostr.nyra@gmail.com>
2026-02-15 16:17:53 +0500

    Седьмое задание полностью готово

---
+++ 7.async/task.js
@@ -0,0 +1,52 @@
+class AlarmClock {
+  constructor() {
+    this.alarmCollection = [];
+    this.intervalId = null;
+  }
+
+  addClock(time, callback) {
+    if (!time || !callback) {
+      throw new Error("Отсутствуют обязательные аргументы");
+    }
+    if (!this.alarmCollection.find(alarm => alarm.time === time)) {
+      console.warn("Уже присутствует звонок на это же время");
+    }
+    this.alarmCollection.push({callback: callback, time: time, canCall: true});
+  }
+
+  removeClock(time) {
+    this.alarmCollection = this.alarmCollection.filter(alarm => alarm.time !== time);
+  }
+
+  getCurrentFormattedTime() {
+    return new Date().toLocaleTimeString().substring(0, 5);
+  }
+
+  start() {
+    if (this.intervalId) {
+      return;
+    }
+    this.intervalId = setInterval(() => {
+      this.alarmCollection.forEach(alarm => {
+        if (alarm.time === this.getCurrentFormattedTime() && alarm.canCall === true) {
+          alarm.canCall = false;
+          alarm.callback();
+        }
+      });
+    }, 1000);
+  }
+
+  stop() {
+    clearInterval(this.intervalId);
+    this.intervalId = null;
+  }
+
+  resetAllCalls() {
+    this.alarmCollection.forEach(alarm => alarm.canCall = true);
+  }
+
+  clearAlarms() {
+    this.stop();
+    this.alarmCollection = [];
+  }
+}
